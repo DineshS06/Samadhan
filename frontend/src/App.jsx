43192ae003f45dea4555456012b32b00dcc18213
DineshS06 <dineshs06@example.com>
2026-07-09 01:14:33 +0530

    Add Vercel SPA rewrite

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,16 @@
-import CitizenPortal from './pages/CitizenPortal'
-import MPDashboard from './pages/MPDashboard'
-import MPLogin from './pages/MPLogin'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import CitizenPortal from "./pages/CitizenPortal";
+import MPLogin from "./pages/MPLogin";
+import MPDashboard from "./pages/MPDashboard";
 
 export default function App() {
-  const path = window.location.pathname
-
-  if (path.startsWith('/mp/login')) {
-    return <MPLogin />
-  }
-
-  if (path.startsWith('/mp')) {
-    return <MPDashboard />
-  }
-
-  return <CitizenPortal />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<CitizenPortal />} />
+        <Route path="/mp/login" element={<MPLogin />} />
+        <Route path="/mp" element={<MPDashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
